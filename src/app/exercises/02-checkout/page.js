b5e6e0432f332ff5ef2aa06bb4e1959b87402ba8
Iryna Howarth <irynahowarth@gmail.com>
2024-04-22 13:42:43 +0100

    checkout 2nd attempt

--- src/app/exercises/02-checkout/page.js
+++ src/app/exercises/02-checkout/page.js
@@ -6,16 +6,23 @@ import reducer from "./reducer";
 import StoreItem from "./StoreItem";
 import CheckoutFlow from "./CheckoutFlow";
 import "./styles.css";
-import Spinner from "../../../components/Spinner/Spinner";
 
 function CheckoutExercise() {
   const [items, dispatch] = React.useReducer(reducer, null);
 
   React.useEffect(() => {
+    const saveItems = window.localStorage.getItem("cart");
     dispatch({
-      type: "initialize-items",
+      type: "initial-items",
+      items: saveItems === null ? [] : JSON.parse(saveItems),
     });
   }, []);
+
+  React.useEffect(() => {
+    if (items !== null) {
+      window.localStorage.setItem("cart", JSON.stringify(items));
+    }
+  }, [items]);
 
   return (
     <>
@@ -36,20 +43,17 @@ function CheckoutExercise() {
             />
           ))}
         </div>
-        {!items ? (
-          <Spinner />
-        ) : (
-          <CheckoutFlow
-            items={items}
-            taxRate={0.15}
-            handleDeleteItem={(item) =>
-              dispatch({
-                type: "delete-item",
-                item,
-              })
-            }
-          />
-        )}
+
+        <CheckoutFlow
+          items={items}
+          taxRate={0.15}
+          handleDeleteItem={(item) =>
+            dispatch({
+              type: "delete-item",
+              item,
+            })
+          }
+        />
       </main>
     </>
   );
--- src/app/exercises/02-checkout/reducer.js
+++ src/app/exercises/02-checkout/reducer.js
@@ -4,18 +4,10 @@ function reducer(state, action) {
   return produce(state, (draftState) => {
     switch (action.type) {
       case "add-item": {
-        window.localStorage.removeItem("cart-" + action.item.id);
         const itemIndex = state.findIndex((item) => item.id === action.item.id);
 
         if (itemIndex !== -1) {
           draftState[itemIndex].quantity += 1;
-          window.localStorage.setItem(
-            "cart-" + action.item.id,
-            JSON.stringify({
-              ...action.item,
-              quantity: draftState[itemIndex].quantity,
-            })
-          );
           return;
         }
 
@@ -23,28 +15,16 @@ function reducer(state, action) {
           ...action.item,
           quantity: 1,
         });
-        window.localStorage.setItem(
-          "cart-" + action.item.id,
-          JSON.stringify({
-            ...action.item,
-            quantity: 1,
-          })
-        );
         return;
       }
-
+      case "initial-items": {
+        return action.items;
+      }
       case "delete-item": {
         const itemIndex = state.findIndex((item) => item.id === action.item.id);
-        window.localStorage.removeItem("cart-" + action.item.id);
 
         draftState.splice(itemIndex, 1);
         return;
-      }
-
-      case "initialize-items": {
-        return Object.keys(localStorage)
-          .filter((key) => key.startsWith("cart-"))
-          .map((key) => JSON.parse(window.localStorage.getItem(key)));
       }
     }
   });
